14247d8ca24502d3940aede6762f27475783a43a
Leandro Guimaraes <leandroguim@hotmail.com>
2023-08-15 12:35:04 -0300

    Add project files.

---
+++ Arquivo3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string sourcePath = @"c:\temp\file1.txt";
+string targetPath = @"c:\temp\file2.txt";
+
+
+try 
+{
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    using (StreamWriter sw = File.AppendText(targetPath))
+    {
+        foreach (string line in lines)
+        {
+            sw.WriteLine(line.ToUpper());
+        }
+    }
+}
+catch(IOException e)
+{
+
+}
